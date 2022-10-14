6b196e4b6760505f75202e3d0c31f8a8d5779ad7
Dmitry Osipovich <dosipovich@hotmail.com>
2022-10-14 15:26:29 +0300

    Задача №27 к 4 семинару решена успешно!

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Сумма цифр числа
+void Sum(string a)
+{
+    int sum = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        sum = sum + (int)Char.GetNumericValue(a[i]);
+    }
+    Console.WriteLine($"Сумма цифр числа {a} = {sum}.");
+}
+
+Console.Write("Введите целое число: ");
+string num = (Console.ReadLine());
+Sum(num);
