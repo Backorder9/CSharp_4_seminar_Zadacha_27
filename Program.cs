892b685a4294179690654ea57099205d0a618f4d
Dmitry Osipovich <dosipovich@hotmail.com>
2022-10-16 10:59:01 +0300

    Функции и методы.

--- Program.cs
+++ Program.cs
@@ -5,16 +5,24 @@
 // 9012 -> 12
 
 // Сумма цифр числа
-void Sum(string a)
+int Sum(string a)
 {
     int sum = 0;
     for (int i = 0; i < a.Length; i++)
     {
         sum = sum + (int)Char.GetNumericValue(a[i]);
     }
-    Console.WriteLine($"Сумма цифр числа {a} = {sum}.");
+    return sum;
+}
+
+void Print(string a, int s)
+{
+    Console.WriteLine($"Сумма цифр числа {a} = {s}.");
+    Console.WriteLine();
 }
 
 Console.Write("Введите целое число: ");
 string num = (Console.ReadLine());
-Sum(num);
+
+int s = Sum(num);
+Print(num, s);
